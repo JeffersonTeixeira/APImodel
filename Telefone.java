4b82cdc0978ee73a592287d70f4397b903aca6b3
JeffersonTeixeira <jeffersonteixeira@outlook.com>
2018-12-07 22:19:49 -0200

    Model

---
+++ Telefone.java
@@ -0,0 +1,37 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Telefone {
+
+    
+    private Long id;
+    private long telefone;
+  //  private Cliente cliente;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+
+
+}
